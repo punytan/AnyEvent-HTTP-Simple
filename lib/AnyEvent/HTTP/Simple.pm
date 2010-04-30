5633f3235d067012f8628ececa8320259e997ac4
punytan <punytan@gmail.com>
2010-04-30 17:29:19 +0900

    initial commit

---
+++ lib/AnyEvent/HTTP/Simple.pm
@@ -0,0 +1,32 @@
+package AnyEvent::HTTP::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::HTTP::Simple -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::HTTP::Simple;
+
+=head1 DESCRIPTION
+
+AnyEvent::HTTP::Simple is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
